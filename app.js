ecdeb9882081de261e17ead2d783121566d43baf
Felipe Lee <felipe.lee.garcia@gmail.com>
2018-09-20 19:39:15 -0500

    Add compression middleware to app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
+var compression = require('compression');
 
 var app = express();
 
@@ -29,6 +30,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
+
+app.use(compression()); // Compress all routes
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
